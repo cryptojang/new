4c243088e9050cfaef099bc144247ec1e6da896b
cryptojang <robert.crypto.jang@gmail.com>
2023-10-24 13:49:42 +0900

    add 02

--- practice231023/main.js
+++ practice231023/main.js
@@ -1,1 +1,10 @@
 console.log("Hi");
+//document -> html
+let h2 = document.createElement("h2");
+//<h2></h2>
+
+let dom = document.getElementById("dom");
+
+h2.innerText = "My name is yurright!";
+
+dom.appendChild(h2);
